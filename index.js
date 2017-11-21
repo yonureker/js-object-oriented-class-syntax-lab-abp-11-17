43297321b26b6fe45e2bc4524bca3bfb7314648d
Onur Eker <yonureker@gmail.com>
2017-11-21 01:06:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+class BoardMember {
+  constructor (name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+    this.veto = function (){
+      console.log(`No, I must disagree`)
+    };
+    this.approve = function(){
+      console.log(`You can do that!`)
+    };
+    this.doCharity = function(){
+      console.log(`I like to help people.`)
+    };
+    this.releasePressStatement = function (){
+      console.log(`You will see great things from Scuber.`)
+    };
+    this.sayHi = function (){
+      console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`)
+    };
+  }
+}
