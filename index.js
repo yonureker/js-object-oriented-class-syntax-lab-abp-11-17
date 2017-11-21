d356da5240fe481d4eab50d13fa028a1314b7d1f
Onur Eker <yonureker@gmail.com>
2017-11-21 01:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ class BoardMember {
       return(`You will see great things from Scuber.`)
     };
     sayHi(){
-      return(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`)
+      return(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
     };
   }
